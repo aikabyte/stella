5902a2b9e5d8c74976b9d29d196fc5b6d288ee98
Aika <aika@aikabyte.com>
2025-11-03 09:37:58 +0200

    refactor: back to square one
    - delete slint files in ui/
    - remove callbacks in main.rs
    
    I want to implement proper MVC architecture.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,7 @@
 slint::include_modules!();
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ui = MainWindow::new()?;
-
-    ui.on_prev_page(|| {
-        println!("called: prev-page()");
-    });
-
-    ui.on_next_page(|| {
-        println!("called: next-page()");
-    });
 
     ui.run()?;
 

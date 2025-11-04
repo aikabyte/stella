4f10b7104a1fd53e8704c9e86aaecd5f4e1515f4
Aika <aika@aikabyte.com>
2025-11-04 21:47:20 +0200

    refactor: switch from Slint to Iced
    Learning Slint's DSL was proving very difficult and using it was not intuitive. Iced provides a much better developer experience despite having less documentation and tooling. We can make swift progress now. The Elm Architecture is more intuitive than MVC.

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Stella
 
 It's a comic reader in early development.
-
-## What's Wrong?
-
-I'm just getting started with software development and this is my first project. Everything is new to me. MVC, Rust, Slint... it's all unfamiliar but I'm figuring it out.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,67 @@
-slint::include_modules!();
+use iced::widget::{Column, button, column, text};
+
+#[derive(Default)]
+struct Counter {
+	value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+	Increment,
+	Decrement,
+	Reset,
+}
+
+impl Counter {
+	fn update(&mut self, message: Message) {
+		match message {
+			Message::Increment => {
+				self.value += 1;
+			}
+			Message::Decrement => {
+				self.value -= 1;
+			}
+			Message::Reset => {
+				self.value = 0;
+			}
+		}
+	}
+
+	fn view(&self) -> Column<'_, Message> {
+		column![
+			button("+").on_press(Message::Increment),
+			text(self.value),
+			button("-").on_press(Message::Decrement),
+			button("reset").on_press(Message::Reset),
+		]
+	}
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ui = MainWindow::new()?;
+	iced::run("Counter", Counter::update, Counter::view)?;
+	Ok(())
+}
 
-    ui.run()?;
+#[cfg(test)]
+mod tests {
+	use crate::Counter;
 
-    Ok(())
+	#[test]
+	fn it_counts_properly() {
+		let mut counter = Counter::default();
+		counter.update(crate::Message::Increment);
+		counter.update(crate::Message::Increment);
+		counter.update(crate::Message::Decrement);
+		assert_eq!(counter.value, 1);
+	}
+
+	#[test]
+	fn it_resets() {
+		let mut counter = Counter::default();
+		counter.update(crate::Message::Increment);
+		counter.update(crate::Message::Increment);
+		counter.update(crate::Message::Decrement);
+		counter.update(crate::Message::Reset);
+		assert_eq!(counter.value, 0);
+	}
 }
